dda07c0c28855219e0c84af054d15d7e12efd059
Agatha Vieira <agatha62207736@alunos.sc.senac.br>
2026-03-26 19:31:17 -0300

    mensagem

---
+++ EXEMPLOIMC.py
@@ -0,0 +1,9 @@
+idade = int(input("Digite a sua idade: "))
+
+if idade < 18:
+    print("Você é menor de idade. ")
+elif idade < 60: 
+    print("Você é adulta. ")
+else: 
+    print("Você é uma pessoa idosa. ")
+    
